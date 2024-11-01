65d197672ee4c546edf7c33779599b1cee5146dd
PedroHenrick1 <pedrohenrick52@gmail.com>
2024-11-01 17:12:28 -0300

    post-funcionando

--- prisma/migrations/20241101184835_create_model_user/migration.sql
+++ prisma/migrations/20241101184835_create_model_user/migration.sql
@@ -1,10 +1,10 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "name" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3),
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
--- server.js
+++ server.js
@@ -1,13 +1,39 @@
-const app = require('./src/app')
+import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const fastify = Fastify({ logger: true });
+
+fastify.get('/users', async (request, reply) => {
+  try {
+    const users = await prisma.user.findMany()
+    reply.send(users);
+  }catch(e) {
+    reply.status(500).send(error);
+  }
+});
+
+fastify.post("/users", async (request, reply) => {
+  const { name, email } = request.body;
+  try {
+    const user = await prisma.user.create({
+      data: { name, email },
+    });
+    reply.send(user);
+  } catch (error) {
+    reply.status(500).send(error);
+  }
+});
 
 const start = async () => {
   try {
-    await app.listen({ port: 3000 })
-    console.log('Servidor rodando na porta 3000')
-  } catch (err) {
-    console.error(err)
-    process.exit(1)
+    await fastify.listen({ port: 3000 });
+    console.log("Servidor rodando em http://localhost:3000");
+  } catch (error) {
+    fastify.log.error(error);
+    process.exit(1);
   }
-}
+};
 
-start()
+start();
